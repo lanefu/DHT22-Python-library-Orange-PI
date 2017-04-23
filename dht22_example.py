7f639c61840b53e93df92034e21d1f39e3e86073
ionutpi <mltrionut@yahoo.co.uk>
2017-04-23 12:33:42 +0200

    Update and rename dht11_example.py to dht22_example.py

--- dht22_example.py
+++ dht22_example.py
@@ -2,7 +2,7 @@ from pyA20.gpio import gpio
 from pyA20.gpio import port
 
 #import RPi.GPIO as GPIO
-import dht11
+import dht22
 import time
 import datetime
 
@@ -15,13 +15,13 @@ gpio.init()
 
 
 # read data using pin 14
-instance = dht11.DHT11(pin=PIN2)
+instance = dht22.DHT22(pin=PIN2)
 
 while True:
     result = instance.read()
     if result.is_valid():
         print("Last valid input: " + str(datetime.datetime.now()))
-        print("Temperature: %d C" % result.temperature)
-        print("Humidity: %d %%" % result.humidity)
+        print("Temperature: %.2f C" % result.temperature)
+        print("Humidity: %.2f %%" % result.humidity)
 
     time.sleep(1)
